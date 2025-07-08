6cf568e8b8524a377c8da219dfe98f9de5ae91c1
satyendra813 <satyendra9173@gmail.com>
2025-07-08 12:57:04 +0530

    chages

--- backend/content-service/src/controllers/notes.controller.js
+++ backend/content-service/src/controllers/notes.controller.js
@@ -16,9 +16,9 @@ mongoose.connection.once("open", () => {
 });
 
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, path.join(__dirname, "../../uploads"));
-	},
+	// destination: function (req, file, cb) {
+	// 	cb(null, path.join(__dirname, "../../uploads"));
+	// },
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
 		cb(null, uniqueSuffix + '-' + file.originalname);
