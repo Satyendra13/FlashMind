fd32259c64c71a566b78efd2683a337f5c618a54
satyendra813 <satyendra9173@gmail.com>
2025-07-22 20:37:04 +0530

    updated file size

--- backend/content-service/src/controllers/notes.controller.js
+++ backend/content-service/src/controllers/notes.controller.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({
 	storage: storage,
-	limits: { fileSize: 50 * 1024 * 1024 }, // 50MB limit
+	limits: { fileSize: 100 * 1024 * 1024 }, // 100MB limit
 	fileFilter: (req, file, cb) => {
 		console.log(file, "file")
 		const allowedTypes = [
